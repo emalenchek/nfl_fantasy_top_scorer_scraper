00bc9d650a0235c901b3c45dbde0a3974cd30cea
Ethan Malenchek <emalenchek@gmail.com>
2019-10-02 13:19:38 -0400

    added total points, positions/teams for each player. Also, exported contents to .csv file (top_25.csv)

--- scrape.py
+++ scrape.py
@@ -3,22 +3,47 @@
 # a program to gather information on the top NFL fantasy scorers
 
 import requests
+import re
 import pandas as pd
 from bs4 import BeautifulSoup
 website_url = requests.get('https://fantasy.nfl.com/research/scoringleaders?position=O&statCategory=stats&statSeason=2019&statType=seasonStats&statWeek=5').text
+path = './top_25.csv'
 
 soup = BeautifulSoup(website_url, 'html.parser') # parses entire page
 
 my_table = soup.find('table',{'class':'tableType-player'}) # finds table of players on page
 
 name_links = my_table.findAll('a',{'class':'playerName'}) # finds all <a> for player names
+points = my_table.findAll('span',{'class':'playerSeasonTotal'}) # stores player points
+positions = my_table.findAll('em')
 
 player_names = [] # instantiate list of player names
+player_points = [] # instantiate list of player points
+player_positions = [] # instantiate list of player positions
+
+count = 0
 
 for link in name_links:
     player_names.append(link.get_text()) # parse links for player names and store in list
 
-df = pd.DataFrame() # Data Frames construction for output formatting
-df['player_names'] = player_names
+for point in points:
+    player_points.append(point.get_text()) # parse spans for player points and store in list
 
-print(df)
+for position in positions:
+    count = count + 1
+    if count > 7: # skips heading <em> elements
+        player_positions.append(position.get_text()) # parse <em> for position/team and store in list
+
+dataf = pd.DataFrame( # construct data frame
+    {'Player Names':player_names,
+     'Total Points':player_points,
+     'Position & Team':player_positions,
+    })
+
+dataf['Player Names'] = player_names
+dataf['Total Points'] = player_points
+dataf['Position & Team'] = player_positions
+
+print(dataf)
+
+dataf.to_csv(path, index = False) # export data frame to csv
